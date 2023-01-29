b351b4a321a9ee3923614006c32b11fd5b3586d3
shunsuke.taga <tagashunsuke@icloud.com>
2023-01-29 16:46:52 +0000

    top page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+(1..100).each do |number|
+  User.create(name: 'user' + number.to_s, email: 'email' + number.to_s + '@test.com', password_digest: 'testtesttest')
+end
+User.create(name: 'user', email: 'email@test.com', password_digest: 'testtesttest')
